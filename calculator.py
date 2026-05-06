a2dde7e8d53b467987f01c1559f8b645458772d0
Shivam <shivamtejpal86@gmail.com>
2026-05-06 13:52:00 +0530

    frame setup

--- calculator.py
+++ calculator.py
@@ -10,3 +10,26 @@ button_values = [
 
 right_symbols = ["÷", "×", "-", "+", "="]
 top_symbols = ["AC", "+/-", "%"]
+
+row_count = len(button_values)
+column_count = len(button_values[0])
+
+color_light_gray ="#D4D4D2"
+color_black ="#1C1C1C"
+color_dark_gray ="#505050"
+color_orange ="#FF9500"
+color_white ="white"
+
+#window setup
+window = tkinter.Tk()
+window.title("Calculator")
+window.resizable(False,False)
+window.mainloop()
+
+frame = tkinter.Frame(window)
+label=tkinter.Label(frame, text="0",font=("Arial",45),background=color_black,foreground=color_white)
+
+label.pack()
+frame.pack()
+
+window.mainloop()
